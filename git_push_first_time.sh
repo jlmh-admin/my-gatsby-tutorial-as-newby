f86b3f91973b4bce9ea669ee6c8f00b719d783ba
jose monroy <webstack.course.test@gmail.com>
2022-10-13 11:20:46 -0500

    first after repository creation

---
+++ git_push_first_time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export GITHUB_USER=jlmh-admin
+export GITHUB_TOKEN=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
+export GITHUB_REPOSITORY=my-gatsby-tutorial-as-newby
+#git remote add origin https://github.com/jlmh-admin/my-gatsby-tutorial-as-newby.git
+#git branch -M main
+#git push -u origin main
